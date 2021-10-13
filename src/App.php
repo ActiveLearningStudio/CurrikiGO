e2d80747cdc1fed53627d60965964bb914aa1955
DESKTOP-V9B7HMN\TK-LPT-0358 <mohsin.iftikhar@tkxel.com>
2021-10-13 16:35:31 +0500

    Condition change for LTI-1.3

--- src/App.php
+++ src/App.php
@@ -100,7 +100,7 @@ class App
                     // encode user information.
                     $lti_submission_info = base64_encode($build_review_data);
 
-                    if (!isset($_SESSION['lti_post']['lti_version'])) {
+                    if (isset($_SESSION['lti']['issuer_client'])) {
                         $grade_params['lti13_extra'] = [
                             'https://canvas.instructure.com/lti/submission' => [
                                 "new_submission" => true,
