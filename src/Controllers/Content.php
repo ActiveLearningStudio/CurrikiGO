585e20716bf14b8970361a7b5ec892ef6e6202c1
AqibYounasAtTkxel <younas.aqib@tkxel.io>
2022-01-18 15:28:09 +0500

    CUR 2534 dynamic full name string

--- src/Controllers/Content.php
+++ src/Controllers/Content.php
@@ -90,9 +90,16 @@ class Content implements ControllerInterface
             $response->send();
         }elseif ( isset($_SESSION['lti']['issuer_client']) ) {
             if ( isset($_SESSION['lti']['user_email']) ){
-                $full_name = $_SESSION['lti']['user_displayname'];
-                $first_name = $full_name[0];
-                $last_name = $full_name[1];
+                $full_name = explode(" ", $_SESSION['lti']['user_displayname']);
+                if(count($full_name) > 1) {
+                    $last_name = array_pop($full_name);
+                    $first_name = implode(" ", $full_name);
+                }
+                else
+                {
+                    $first_name = $full_name;
+                    $last_name = " ";
+                }
                 $custom_email_id = $_SESSION['lti']['user_email'];
             }
             else{
