3781235c7f93cba77b5e97f202dff3bbe06d5666
AqibYounasAtTkxel <94671546+AqibYounasAtTkxel@users.noreply.github.com>
2022-04-14 16:36:11 +0500

    CUR 3554 lti/content params syntax correction

--- src/Controllers/Content.php
+++ src/Controllers/Content.php
@@ -84,7 +84,7 @@ class Content implements ControllerInterface
             $lms_url = parse_url($content_item_return_url, PHP_URL_SCHEME)
                         .'://'.parse_url($content_item_return_url, PHP_URL_HOST).$port;
             
-            $studio_url = CURRIKI_STUDIO_HOST.'/lti/content/'.urlencode($lms_url).'/'.$oauth_consumer_key.'/'.urlencode($redirect_url).'/'.$course_id.'/'.$api_domain_url;
+            $studio_url = CURRIKI_STUDIO_HOST.'/lti/content/'.urlencode($lms_url).'/'.$oauth_consumer_key.'/'.urlencode($redirect_url).'&course_id='.$course_id.'&api_domain_url='.$api_domain_url;
             if (!empty($custom_email_id)) {
                 $studio_url .= '?user_email=' . urlencode($custom_email_id);
             } else{
@@ -137,7 +137,7 @@ class Content implements ControllerInterface
             $course_id = ParamValidate::getKeyInCustomFields($_SESSION, 'course_id');
             $api_domain_url = ParamValidate::getKeyInCustomFields($_SESSION, 'api_domain_url');
 
-            $studio_url = CURRIKI_STUDIO_HOST.'/lti/content/'.urlencode($lms_url).'/'.$lti_client_id.'/'.urlencode($redirect_url).'/'.$course_id.'/'.$api_domain_url;
+            $studio_url = CURRIKI_STUDIO_HOST.'/lti/content/'.urlencode($lms_url).'/'.$lti_client_id.'/'.urlencode($redirect_url).'&course_id='.$course_id.'&api_domain_url='.$api_domain_url;
             if (!empty($custom_email_id)) {
                 $studio_url .= '?user_email=' . urlencode($custom_email_id);
             }
